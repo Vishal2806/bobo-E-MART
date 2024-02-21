328cafeaa41b694a534bef9be572899111024fb2
vishal2806 <vishal.v2806@gmail.com>
2024-02-21 13:15:36 +0530

    updating folder/files

--- ecommerce-frontend/src/components/header.tsx
+++ ecommerce-frontend/src/components/header.tsx
@@ -1,35 +1,64 @@
 import { Link } from "react-router-dom";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaUser,
+  FaSignOutAlt,
+} from "react-icons/fa";
 import { useState } from "react";
-import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { User } from "../types/types";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import toast from "react-hot-toast";
 
-const user = { _id: "abcd", role: "admin" };
-const Header = () => {
-    const [isOpen,setIsOpen] = useState<boolean>(false);
-    const logoutHandler = ()=>{
-        setIsOpen(false);
-    };
+interface PropsType {
+  user: User | null;
+}
+
+const Header = ({ user }: PropsType) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const logoutHandler = async () => {
+    try {
+      await signOut(auth);
+      toast.success("Sign Out Successfully");
+      setIsOpen(false);
+    } catch (error) {
+      toast.error("Sign Out Fail");
+    }
+  };
+
   return (
     <nav className="header">
-      <Link  onClick ={()=> setIsOpen(false) }to={"/"}>Home</Link>
-      <Link onClick ={()=> setIsOpen(false) } to={"/search"}>
+      <Link onClick={() => setIsOpen(false)} to={"/"}>
+        HOME
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/search"}>
         <FaSearch />
       </Link>
-      <Link onClick ={()=> setIsOpen(false) } to="/cart">
+      <Link onClick={() => setIsOpen(false)} to={"/cart"}>
         <FaShoppingBag />
       </Link>
+
       {user?._id ? (
         <>
-          <button onClick ={()=>setIsOpen((prev) =>!prev)}>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
             <FaUser />
           </button>
-          <dialog open ={isOpen}>
+          <dialog open={isOpen}>
             <div>
               {user.role === "admin" && (
-                <Link onClick ={()=> setIsOpen(false) }to="/admin/dashboard"> Admin </Link>
+                <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
+                  Admin
+                </Link>
               )}
-              <Link onClick ={()=> setIsOpen(false) }to="/orders">Orders</Link>
+
+              <Link onClick={() => setIsOpen(false)} to="/orders">
+                Orders
+              </Link>
               <button onClick={logoutHandler}>
-                <FaSignOutAlt/>
+                <FaSignOutAlt />
               </button>
             </div>
           </dialog>
@@ -41,6 +70,6 @@ const Header = () => {
       )}
     </nav>
   );
-}
+};
 
 export default Header;
