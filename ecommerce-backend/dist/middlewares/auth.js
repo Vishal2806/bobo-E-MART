77453d580deacd7f6a6da7bd572de91fc8ee1cff
vishal2806 <vishal.v2806@gmail.com>
2024-02-29 10:28:05 +0530

    updated

--- ecommerce-backend/dist/middlewares/auth.js
+++ ecommerce-backend/dist/middlewares/auth.js
@@ -5,11 +5,11 @@ import { TryCatch } from "./error.js";
 export const adminOnly = TryCatch(async (req, res, next) => {
     const { id } = req.query;
     if (!id)
-        return next(new ErrorHandler("Login first", 401));
+        return next(new ErrorHandler("Saale Login Kr phle", 401));
     const user = await User.findById(id);
     if (!user)
-        return next(new ErrorHandler("Id not matched", 401));
+        return next(new ErrorHandler("Saale Fake ID Deta Hai", 401));
     if (user.role !== "admin")
-        return next(new ErrorHandler("You don't have a credential", 403));
+        return next(new ErrorHandler("Saale Aukat Nhi Hai Teri", 403));
     next();
 });
